66a897b9803404790f7d4d730508c7eaf3751eb2
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-14 23:00:26 -0700

    Add more contrast to lunar lighting

--- client/aframe/components/MoonScene.js
+++ client/aframe/components/MoonScene.js
@@ -18,12 +18,12 @@ class MoonScene extends React.Component {
 
       <a-sky src='#moon' rotation='60 0 0' />
 
-      <a-light type='directional' color='#AAA' position='5 5 -2' intensity='1' />
+      <a-light type='directional' color='#AAA' position='5 5 -2' intensity='2' />
       <a-light type='ambient' color='#AAA' intensity='0.05' />
 
 
       <a-plane id='ground' src='#ground' material='side:double' 
-        position='0 0 -3' rotation='-90 0 0' width='10' height='10' />
+        position='2 0 -3' rotation='-90 0 0' width='10' height='10' />
 
       <a-entity sound="src: /assets/moonsounds.mp3; autoplay: true; loop: true; volume: 10;" position='0 1.5 0'>
         <a-camera id='camera' wasd-controls-enabled='true'/>
